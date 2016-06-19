0619359ac23dad8b4853a5d519d0d5f73065eef4
Ian Baldo <ianblopes@gmail.com>
2016-06-19 02:11:10 -0300

    Limited FPS + Added ClicknDrag to Nodes

--- Editor/Main.py
+++ Editor/Main.py
@@ -1,4 +1,5 @@
 import pygame, sys
+from pygame import mouse
 from pygame.locals import *
 import Node
 import Zoom
@@ -11,6 +12,9 @@ pygame.display.set_caption("Editor")
 
 # Initializes pygame
 pygame.init()
+
+# Clock
+clock = pygame.time.Clock()
 
 # Initialize Zoom Module
 Zoom.init(window_width,window_height,100,100)
@@ -26,7 +30,7 @@ BLUE = (0,0,255)
 nodeList = []
 nodeList.append(Node.NodeClass(1,50,50))
 nodeList.append(Node.NodeClass(2,0,0))
-nodeList.append(Node.NodeClass(3,10,40))
+nodeList.append(Node.NodeClass(3,10,5))
 nodeList.append(Node.NodeClass(4,100,100))
 
 
@@ -35,21 +39,42 @@ while True:
     DISPLAYsurf.fill(WHITE)
 
     for node in nodeList:
-        node.draw(DISPLAYsurf,BLUE)
+        node.draw(DISPLAYsurf)
+
+    for node in nodeList:
+        if node.isSelected():
+            mouse_pos = mouse.get_pos()
+            node.followCursor(mouse_pos)
 
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = mouse.get_pos()
+            for node in nodeList:
+                node.clicked(mouse_pos)
+
+        elif event.type == pygame.MOUSEBUTTONUP:
+            for node in nodeList:
+                if node.isSelected():
+                    node.released()
+
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
                 Zoom.zoomIn()
                 for node in nodeList:
                     node.updateCoords()
+
             elif event.key == pygame.K_DOWN:
                 Zoom.zoomOut()
                 for node in nodeList:
                     node.updateCoords()
+
             print ("Zoom:%f" %(Zoom.zoom))
-           # print ("X:%d ; Y:%d ; Width:%d ; Height:%d" % ())
+            
+    # Updates the display
     pygame.display.update()
+    # Limits FPS to 60
+    clock.tick(60)
