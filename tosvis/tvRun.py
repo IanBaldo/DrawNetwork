db2bc4e1c2f5ddc6f03bfee59cce5bce2c250b76
Ian Baldo <ianblopes@gmail.com>
2017-06-24 17:36:37 -0300

    Fix path + rename

--- tosvis/tvRun.py
+++ tosvis/tvRun.py
@@ -10,17 +10,9 @@ class MyNode(Node):
     def animateAmRecv(self,time,amtype,amlen):
             Node.animateAmRecv(self,time,amtype,amlen)
 
-###############################
-tv = TosVis(100, showDebug=False, file='/home/terra/Documents/ProjetoFinal/drawnetwork.json')
+############################### 
+tv = TosVis(100, showDebug=False, file='/home/terra/Documents/DrawNetwork/drawnetwork.json')
 MONITORED_AM = [0x71]
 
-# for x in range(4):
-#     for y in range(4):
-#         pos = (100+80*x+random()*40,100+80*y+random()*40)
-#         node = MyNode(pos, txRange=100)
-#         node.monitoredAm = MONITORED_AM
-#         tv.addNode(node)
-
 tv.addNetworkNodes()
-
 tv.run()
